77e272181e580280129ade92f1ff238e5f24b7bb
EC2 Default User <ec2-user@ip-10-0-7-237.ec2.internal>
2023-09-19 14:57:46 +0000

    do it right

--- Modules/compute/alb.tf
+++ Modules/compute/alb.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 # Define a condition to control resource creation based on the number of subnets
-resource "aws_lb" "igor_alb" {
+resource "aws_lb" "alb" {
   count              = local.is_single_subnet ? 0 : 1
   name               = "Igor-alb"
   internal           = false
@@ -17,9 +17,9 @@ resource "aws_lb" "igor_alb" {
   }
 }
 
-resource "aws_lb_target_group" "Igor-lb-tg" {
+resource "aws_lb_target_group" "lb_tg" {
   count = local.is_single_subnet ? 0 : 1
-  name     = "Igor-lb-tg"
+  name     = "igors-lb-tg"
   port     = var.Port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
@@ -29,21 +29,21 @@ resource "aws_lb_target_group" "Igor-lb-tg" {
   }
 }
 
-resource "aws_lb_listener" "igor_listener" {
+resource "aws_lb_listener" "listener" {
   count              = local.is_single_subnet ? 0 : 1
-  load_balancer_arn = aws_lb.igor_alb[0].arn
+  load_balancer_arn = aws_lb.alb[0].arn
   port              = var.Port
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.Igor-lb-tg[0].arn
+    target_group_arn = aws_lb_target_group.lb_tg[0].arn
   }
 }
 
 resource "aws_lb_target_group_attachment" "target_group_attachment_1" {
   count            = local.is_single_subnet ? 0 : var.instanceCount
-  target_group_arn = aws_lb_target_group.Igor-lb-tg[0].arn
+  target_group_arn = aws_lb_target_group.lb_tg[0].arn
   target_id        = aws_instance.igor-Easy[count.index].id
   port             = var.Port
 }
--- Modules/network/subnets.tf
+++ Modules/network/subnets.tf
@@ -1,9 +1,9 @@
-resource "aws_internet_gateway" "my_igw" {
+resource "aws_internet_gateway" "internet_gw" {
 
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_subnet" "igor_pub_terra" {
+resource "aws_subnet" "public" {
   count                   = var.SubnetCount
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.subnet_cidr[count.index]
@@ -14,17 +14,17 @@ resource "aws_subnet" "igor_pub_terra" {
   }
 }
 
-resource "aws_route_table" "igor_terra_route" {
+resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block = var.allTraffic
-    gateway_id = aws_internet_gateway.my_igw.id
+    gateway_id = aws_internet_gateway.internet_gw.id
   }
 }
 
-resource "aws_route_table_association" "public_subnet_1_association" {
+resource "aws_route_table_association" "public_subnet_association" {
   count          = var.SubnetCount
-  subnet_id      = aws_subnet.igor_pub_terra[count.index].id
-  route_table_id = aws_route_table.igor_terra_route.id
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.route_table.id
 }
